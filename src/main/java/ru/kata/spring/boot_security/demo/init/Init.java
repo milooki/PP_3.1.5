9c2abc218495c89eb8b6fcb7987d5e7cb11fadf7
Linar Mykha <lendramykha@mail.ru>
2023-06-29 23:07:41 +0300

    second

--- src/main/java/ru/kata/spring/boot_security/demo/init/Init.java
+++ src/main/java/ru/kata/spring/boot_security/demo/init/Init.java
@@ -2,7 +2,7 @@ package ru.kata.spring.boot_security.demo.init;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.kata.spring.boot_security.demo.Repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
